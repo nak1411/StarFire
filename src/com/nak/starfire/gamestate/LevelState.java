9d3adcedbada8d64c627d31d79e6bb74d2ca2f7b
nak1411 <nak1411@gmail.com>
2015-09-15 17:08:04 -0700

    Modified spritesheets and entities

--- src/com/nak/starfire/gamestate/LevelState.java
+++ src/com/nak/starfire/gamestate/LevelState.java
@@ -14,11 +14,13 @@ public class LevelState {
 	private int width, height;
 	private int[][] tiles;
 	private int dX, dY;
+	private int xSpawn = (Game.WIDTH * Game.SCALE / 2) - 12;
+	private int ySpawn = (Game.HEIGHT * Game.SCALE / 2) - 24;
 	private int xVel = 2;
 	private int yVel = 2;
 
 	public LevelState(String path) {
-		player = new Player((Game.WIDTH * Game.SCALE / 2) - 12, (Game.HEIGHT * Game.SCALE / 2) - 24);
+		player = new Player(xSpawn, ySpawn);
 		loadLevel(path);
 	}
 
@@ -35,7 +37,7 @@ public class LevelState {
 				getTile(x, y).render(g, (x * Tile.TILEWIDTH) - dX, (y * Tile.TILEHEIGHT) - dY);
 			}
 		}
-		player.render(g);
+		player.render(g, xSpawn, ySpawn);
 	}
 
 	public Tile getTile(int x, int y) {
